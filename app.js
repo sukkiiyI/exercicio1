fdcd00c7beea06166f16a37f6e60fa5c3aff6a82
sukkiiy <oemanuelly360@gmail.com>
2025-02-05 22:12:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+alert("Boas vindas ao jogo do número secreto");
+let numeroSecreto = 19;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual a 19
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número de 1 a 30');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1
+        tentativas++;
+    } 
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
